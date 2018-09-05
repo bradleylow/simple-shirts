abc66c66c595a6ad1045ca4994bb697697d8cae8
Bradley Low <bradleylow93@gmail.com>
2018-09-05 11:14:04 -0700

    Hooked up project to Firebase database to pull in products.

--- src/containers/Shop/Shop.js
+++ src/containers/Shop/Shop.js
@@ -3,48 +3,35 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Aux/Aux';
 import ProductItems from '../../components/ProductItems/ProductItems';
 
+import axios from 'axios';
+
 class Shop extends Component {
     state = {
-        products: [
-            {
-                id: 100001,
-                name: 'Blue SS',
-                price: '14.95',
-                image: 'blue-shirt.jpg'
-            },
-            {
-                id: 100002,
-                name: 'Cobalt SS',
-                price: '14.95',
-                image: 'cobalt-shirt.jpg'
-            },
-            {
-                id: 100003,
-                name: 'Green SS',
-                price: '14.95',
-                image: 'green-shirt.jpg'
-            },
-            {
-                id: 100004,
-                name: 'Grey SS',
-                price: '14.95',
-                image: 'grey-shirt.jpg'
-            },
-            {
-                id: 100005,
-                name: 'Purple SS',
-                price: '14.95',
-                image: 'purple-shirt.jpg'
-            }
-        ]
+        products: null
+    }
+
+    componentDidMount() {
+        axios.get('https://simple-shirts.firebaseio.com/products.json')
+            .then( response => {
+                this.setState({ products: response.data });
+            });
     }
 
     render () {
+        let productItems = null;
+
+        if (this.state.products) {
+            productItems = (
+                <Aux>
+                    <ProductItems products={this.state.products} />
+                </Aux>
+            )
+        }
+
         return (
             <Aux>
-                <ProductItems products={this.state.products} />
+                {productItems}
             </Aux>
-
         );
     }
 }
