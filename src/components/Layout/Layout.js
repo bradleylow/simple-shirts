8c3eb3edd958dca8411474ed7df2115b3bc05375
Bradley Low <bradleylow93@gmail.com>
2018-09-04 17:07:53 -0700

    Removed placeholder content from Layout component.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,7 +5,7 @@ import Aux from '../../hoc/Aux/Aux';
 const layout = (props) => (
     <Aux>
         <header className="container mx-auto">
-            <p>Toolbar, SlideMenu</p>
+            
         </header>
         <main className="container mx-auto">
             {props.children}
