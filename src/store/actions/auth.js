f17e5ec4a2f0433156ab9b23009bb865adf50084
Bradley Low <bradleylow93@gmail.com>
2018-10-06 14:05:39 -0700

    Remove console log in auth action

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -70,7 +70,6 @@ export const auth = (email, password, isLogin) => {
                 dispatch(checkAuthTimeout(response.data.expiresIn));
             })
             .catch(err => {
-                console.log(err);
                 dispatch(authFail(err.response.data.error));
             })
     };
