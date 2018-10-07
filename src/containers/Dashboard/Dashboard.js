5adc76b1aae746cfd98632d511cc839dc00214d1
Bradley Low <bradleylow93@gmail.com>
2018-10-07 16:31:18 -0700

    Added error handling for dashboard orders request.

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -11,7 +11,8 @@ import * as actions from '../../store/actions/index';
 class Dashboard extends Component {
 
     state = {
-        orders: []
+        orders: [],
+        ordersError: false
     }
 
     componentWillMount () {
@@ -24,17 +25,20 @@ class Dashboard extends Component {
         const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"';
 
         axios.get('/order.json' + queryParams)
-        .then(response => {
+            .then(response => {
 
-            for (let key in response.data) {
-                orders.push(response.data[key]);
-            }
+                for (let key in response.data) {
+                    orders.push(response.data[key]);
+                }
 
-            // Sort orders by order id DESC
-            orders.sort((a, b) => b.id - a.id).slice(0, 3);
+                // Sort orders by order id DESC
+                orders.sort((a, b) => b.id - a.id).slice(0, 3);
 
-            this.setState({ orders: orders });
-        })
+                this.setState({ orders: orders });
+            })
+            .catch(error => {
+                this.setState({ ordersError: true })
+            });
     }
 
     logoutHandler = () => {
@@ -43,7 +47,27 @@ class Dashboard extends Component {
         this.props.history.push('/');
     }
 
+    dismissError = () => {
+        this.setState({ ordersError: false });
+    }
+
     render () {
+        let ordersError = null;
+
+        if (this.state.ordersError === true) {
+            ordersError = (
+                <div className="modal modal--error">
+                    <div className="modal__content">
+                        <h5 className="text-center mb-8">Something went wrong!</h5>
+                        <p>Your orders could not be found at the moment. Please refresh the page or view at a later time.</p>
+                        <div className="button-wrapper mt-12 text-center">
+                            <button className="button button--small button--blue" onClick={this.dismissError}>Dismiss</button>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+
         let orderItems = (
             <div className="empty-orders my-12 text-center">
                 <h3 className="mb-12">You currently do not have any orders.</h3>
@@ -59,6 +83,7 @@ class Dashboard extends Component {
 
         return (
             <div className="dashboard-page dashboard">
+                {ordersError}
                 <div className="dashboard__intro">
                     <h3 className="mb-6">Welcome to your Dashboard!</h3>
                     <p>You are logged in as <span className="font-medium">{this.props.userEmail}</span></p>
